61cd8bc6d083ce45c94d3ea1a85da459f78ed259
Jeremy J Starcher <jeremy.starcher@gmail.com>
2020-04-14 02:47:36 -0400

    The window resizes properly and can be closed.

--- src/sdl2/main.c
+++ src/sdl2/main.c
@@ -47,6 +47,7 @@
 #define CANVAS_WIDTH GRAPHICS_WIDTH
 #define CANVAS_HEIGHT GRAPHICS_HEIGHT
 
+UBYTE quit = FALSE;
 SDL_Window *window;
 SDL_Renderer *renderer;
 
@@ -70,7 +71,8 @@ int PLATFORM_Initialise(int *argc, char *argv[])
 	window = SDL_CreateWindow("SDL_ttf in SDL2",
 							  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 							  CANVAS_WIDTH, CANVAS_HEIGHT,
-							  0);
+							  SDL_WINDOW_RESIZABLE /* | SDL_WINDOW_OPENGL */ | SDL_WINDOW_SHOWN);
+
 	renderer = SDL_CreateRenderer(window, -1, 0);
 
 	if (window == NULL)
@@ -95,31 +97,98 @@ int PLATFORM_Exit(int run_monitor)
 
 void PLATFORM_DisplayScreen(void)
 {
-	UBYTE pixels[CANVAS_WIDTH * CANVAS_HEIGHT * 4];
+	int x, y;
+	int window_width, window_height;
+	int render_width, render_height;
+	SDL_Event e;
+	SDL_Texture *texture;
+	int VIDEOMODE_src_offset_left = 0;
+	int VIDEOMODE_src_offset_top = 0;
+	UBYTE *screen;
+	float canvasRatio;
+	float windowRatio;
+	static unsigned char pixels[CANVAS_WIDTH * CANVAS_HEIGHT * 4];
 
-	SDL_Texture *texture = SDL_CreateTexture(
+	SDL_StartTextInput();
+
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+		{
+			quit = TRUE;
+		}
+
+		if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+		{
+#if 0
+			switch (e.key.keysym.sym)
+			{
+			case SDLK_RETURN:
+				input.keycode = AKEY_RETURN;
+				break;
+			case SDLK_LEFT:
+				input.keycode = AKEY_LEFT;
+				break;
+			case SDLK_RIGHT:
+				input.keycode = AKEY_RIGHT;
+				break;
+			case SDLK_UP:
+				input.keycode = AKEY_UP;
+				break;
+			case SDLK_DOWN:
+				input.keycode = AKEY_DOWN;
+				break;
+			}
+#endif
+		}
+
+#if 0
+
+		if (e.type == SDL_TEXTINPUT)
+		{
+			UBYTE khar = (UBYTE)e.text.text[0];
+			input.keycode = ascii2scan(khar);
+			input.shift = false;
+			input.control = false;
+		}
+#endif
+	}
+
+	SDL_GetWindowSize(window, &window_width, &window_height);
+
+	canvasRatio = (float)GRAPHICS_HEIGHT / (float)GRAPHICS_WIDTH;
+	windowRatio = (float)window_height / (float)window_width;
+
+	if (windowRatio < canvasRatio)
+	{
+		render_height = window_height;
+		render_width = render_height / canvasRatio;
+	}
+	else
+	{
+		render_width = window_width;
+		render_height = render_width * canvasRatio;
+	}
+
+	SDL_RenderClear(renderer);
+	texture = SDL_CreateTexture(
 		renderer,
 		SDL_PIXELFORMAT_ARGB8888,
 		SDL_TEXTUREACCESS_STREAMING,
 		GRAPHICS_WIDTH, GRAPHICS_HEIGHT);
 
-	int VIDEOMODE_src_offset_left = 0;
-	int VIDEOMODE_src_offset_top = 0;
-
-	UBYTE *screen = (UBYTE *)Screen_atari + Screen_WIDTH * VIDEOMODE_src_offset_top + VIDEOMODE_src_offset_left;
-
-	int x, y;
+	screen = (UBYTE *)Screen_atari + Screen_WIDTH * VIDEOMODE_src_offset_top + VIDEOMODE_src_offset_left;
 
 	screen += 384 * 24 + 24;
 	for (y = 0; y < GRAPHICS_HEIGHT; y++)
 	{
 		for (x = 0; x < GRAPHICS_WIDTH; x++)
 		{
-			UBYTE c = screen[x];
+			unsigned char c = screen[x];
 
-			UBYTE r = Colours_GetR(c);
-			UBYTE b = Colours_GetB(c);
-			UBYTE g = Colours_GetG(c);
+			unsigned char r = Colours_GetR(c);
+			unsigned char b = Colours_GetB(c);
+			unsigned char g = Colours_GetG(c);
 
 			const unsigned int offset = (GRAPHICS_WIDTH * 4 * y) + x * 4;
 			pixels[offset + 0] = b;
@@ -136,412 +205,13 @@ void PLATFORM_DisplayScreen(void)
 		&pixels[0],
 		GRAPHICS_WIDTH * 4);
 
+	SDL_RenderSetLogicalSize(renderer, render_width, render_height);
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
-
 	SDL_RenderPresent(renderer);
 }
 
 int PLATFORM_Keyboard(void)
 {
-	/* int keycode = getch(); */
-	int keycode = 33;
-
-	INPUT_key_consol = INPUT_CONSOL_NONE;
-
-	switch (keycode)
-	{
-	case 0x01:
-		keycode = AKEY_CTRL_a;
-		break;
-	case 0x02:
-		keycode = AKEY_CTRL_b;
-		break;
-	case 0x03:
-		keycode = AKEY_CTRL_c;
-		break;
-	case 0x04:
-		keycode = AKEY_CTRL_d;
-		break;
-	case 0x05:
-		keycode = AKEY_CTRL_e;
-		break;
-	case 0x06:
-		keycode = AKEY_CTRL_f;
-		break;
-	case 0x07:
-		keycode = AKEY_CTRL_g;
-		break;
-		/*
-	case 0x08:
-		keycode = AKEY_CTRL_h;
-		break;
- */
-	case 0x09:
-		keycode = AKEY_TAB;
-		break;
-		/*
-	case 0x0a:
-		keycode = AKEY_CTRL_j;
-		break;
- */
-	case 0x0b:
-		keycode = AKEY_CTRL_k;
-		break;
-	case 0x0c:
-		keycode = AKEY_CTRL_l;
-		break;
-		/*
-	case 0x0d:
-		keycode = AKEY_CTRL_m;
-		break;
- */
-	case 0x0e:
-		keycode = AKEY_CTRL_n;
-		break;
-	case 0x0f:
-		keycode = AKEY_CTRL_o;
-		break;
-	case 0x10:
-		keycode = AKEY_CTRL_p;
-		break;
-	case 0x11:
-		keycode = AKEY_CTRL_q;
-		break;
-	case 0x12:
-		keycode = AKEY_CTRL_r;
-		break;
-	case 0x13:
-		keycode = AKEY_CTRL_s;
-		break;
-	case 0x14:
-		keycode = AKEY_CTRL_t;
-		break;
-	case 0x15:
-		keycode = AKEY_CTRL_u;
-		break;
-	case 0x16:
-		keycode = AKEY_CTRL_v;
-		break;
-	case 0x17:
-		keycode = AKEY_CTRL_w;
-		break;
-	case 0x18:
-		keycode = AKEY_CTRL_x;
-		break;
-	case 0x19:
-		keycode = AKEY_CTRL_y;
-		break;
-	case 0x1a:
-		keycode = AKEY_CTRL_z;
-		break;
-	case '`':
-		keycode = AKEY_CAPSTOGGLE;
-		break;
-	case '!':
-		keycode = AKEY_EXCLAMATION;
-		break;
-	case '"':
-		keycode = AKEY_DBLQUOTE;
-		break;
-	case '#':
-		keycode = AKEY_HASH;
-		break;
-	case '$':
-		keycode = AKEY_DOLLAR;
-		break;
-	case '%':
-		keycode = AKEY_PERCENT;
-		break;
-	case '&':
-		keycode = AKEY_AMPERSAND;
-		break;
-	case '\'':
-		keycode = AKEY_QUOTE;
-		break;
-	case '@':
-		keycode = AKEY_AT;
-		break;
-	case '(':
-		keycode = AKEY_PARENLEFT;
-		break;
-	case ')':
-		keycode = AKEY_PARENRIGHT;
-		break;
-	case '[':
-		keycode = AKEY_BRACKETLEFT;
-		break;
-	case ']':
-		keycode = AKEY_BRACKETRIGHT;
-		break;
-	case '<':
-		keycode = AKEY_LESS;
-		break;
-	case '>':
-		keycode = AKEY_GREATER;
-		break;
-	case '=':
-		keycode = AKEY_EQUAL;
-		break;
-	case '?':
-		keycode = AKEY_QUESTION;
-		break;
-#ifdef PADMINUS
-	case PADMINUS:
-#endif
-	case '-':
-		keycode = AKEY_MINUS;
-		break;
-#ifdef PADPLUS
-	case PADPLUS:
-#endif
-	case '+':
-		keycode = AKEY_PLUS;
-		break;
-#ifdef PADSTAR
-	case PADSTAR:
-#endif
-	case '*':
-		keycode = AKEY_ASTERISK;
-		break;
-#ifdef PADSLASH
-	case PADSLASH:
-#endif
-	case '/':
-		keycode = AKEY_SLASH;
-		break;
-	case ':':
-		keycode = AKEY_COLON;
-		break;
-	case ';':
-		keycode = AKEY_SEMICOLON;
-		break;
-	case ',':
-		keycode = AKEY_COMMA;
-		break;
-	case '.':
-		keycode = AKEY_FULLSTOP;
-		break;
-	case '_':
-		keycode = AKEY_UNDERSCORE;
-		break;
-	case '^':
-		keycode = AKEY_CIRCUMFLEX;
-		break;
-	case '\\':
-		keycode = AKEY_BACKSLASH;
-		break;
-	case '|':
-		keycode = AKEY_BAR;
-		break;
-	case ' ':
-		keycode = AKEY_SPACE;
-		break;
-	case '0':
-		keycode = AKEY_0;
-		break;
-	case '1':
-		keycode = AKEY_1;
-		break;
-	case '2':
-		keycode = AKEY_2;
-		break;
-	case '3':
-		keycode = AKEY_3;
-		break;
-	case '4':
-		keycode = AKEY_4;
-		break;
-	case '5':
-		keycode = AKEY_5;
-		break;
-	case '6':
-		keycode = AKEY_6;
-		break;
-	case '7':
-		keycode = AKEY_7;
-		break;
-	case '8':
-		keycode = AKEY_8;
-		break;
-	case '9':
-		keycode = AKEY_9;
-		break;
-	case 'a':
-		keycode = AKEY_a;
-		break;
-	case 'b':
-		keycode = AKEY_b;
-		break;
-	case 'c':
-		keycode = AKEY_c;
-		break;
-	case 'd':
-		keycode = AKEY_d;
-		break;
-	case 'e':
-		keycode = AKEY_e;
-		break;
-	case 'f':
-		keycode = AKEY_f;
-		break;
-	case 'g':
-		keycode = AKEY_g;
-		break;
-	case 'h':
-		keycode = AKEY_h;
-		break;
-	case 'i':
-		keycode = AKEY_i;
-		break;
-	case 'j':
-		keycode = AKEY_j;
-		break;
-	case 'k':
-		keycode = AKEY_k;
-		break;
-	case 'l':
-		keycode = AKEY_l;
-		break;
-	case 'm':
-		keycode = AKEY_m;
-		break;
-	case 'n':
-		keycode = AKEY_n;
-		break;
-	case 'o':
-		keycode = AKEY_o;
-		break;
-	case 'p':
-		keycode = AKEY_p;
-		break;
-	case 'q':
-		keycode = AKEY_q;
-		break;
-	case 'r':
-		keycode = AKEY_r;
-		break;
-	case 's':
-		keycode = AKEY_s;
-		break;
-	case 't':
-		keycode = AKEY_t;
-		break;
-	case 'u':
-		keycode = AKEY_u;
-		break;
-	case 'v':
-		keycode = AKEY_v;
-		break;
-	case 'w':
-		keycode = AKEY_w;
-		break;
-	case 'x':
-		keycode = AKEY_x;
-		break;
-	case 'y':
-		keycode = AKEY_y;
-		break;
-	case 'z':
-		keycode = AKEY_z;
-		break;
-	case 'A':
-		keycode = AKEY_A;
-		break;
-	case 'B':
-		keycode = AKEY_B;
-		break;
-	case 'C':
-		keycode = AKEY_C;
-		break;
-	case 'D':
-		keycode = AKEY_D;
-		break;
-	case 'E':
-		keycode = AKEY_E;
-		break;
-	case 'F':
-		keycode = AKEY_F;
-		break;
-	case 'G':
-		keycode = AKEY_G;
-		break;
-	case 'H':
-		keycode = AKEY_H;
-		break;
-	case 'I':
-		keycode = AKEY_I;
-		break;
-	case 'J':
-		keycode = AKEY_J;
-		break;
-	case 'K':
-		keycode = AKEY_K;
-		break;
-	case 'L':
-		keycode = AKEY_L;
-		break;
-	case 'M':
-		keycode = AKEY_M;
-		break;
-	case 'N':
-		keycode = AKEY_N;
-		break;
-	case 'O':
-		keycode = AKEY_O;
-		break;
-	case 'P':
-		keycode = AKEY_P;
-		break;
-	case 'Q':
-		keycode = AKEY_Q;
-		break;
-	case 'R':
-		keycode = AKEY_R;
-		break;
-	case 'S':
-		keycode = AKEY_S;
-		break;
-	case 'T':
-		keycode = AKEY_T;
-		break;
-	case 'U':
-		keycode = AKEY_U;
-		break;
-	case 'V':
-		keycode = AKEY_V;
-		break;
-	case 'W':
-		keycode = AKEY_W;
-		break;
-	case 'X':
-		keycode = AKEY_X;
-		break;
-	case 'Y':
-		keycode = AKEY_Y;
-		break;
-	case 'Z':
-		keycode = AKEY_Z;
-		break;
-	case 0x1b:
-		keycode = AKEY_ESCAPE;
-		break;
-	case 8:
-	case 127:
-#ifdef KEY_BACKSPACE
-#if KEY_BACKSPACE != 8 && KEY_BACKSPACE != 127
-	case KEY_BACKSPACE:
-#endif
-#endif
-		keycode = AKEY_BACKSPACE;
-		break;
-	case '\n':
-		keycode = AKEY_RETURN;
-		break;
-	default:
-		keycode = AKEY_NONE;
-		break;
-	}
-	return keycode;
 }
 
 int PLATFORM_PORT(int num)
@@ -561,7 +231,7 @@ int main(int argc, char **argv)
 		return 3;
 
 	/* main loop */
-	for (;;)
+	while (!quit)
 	{
 		INPUT_key_code = PLATFORM_Keyboard();
 		Atari800_Frame();
